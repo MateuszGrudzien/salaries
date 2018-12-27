9c1b7010b1499d637cbcb7921a73b44aa45f0e2c
Mateusz Grudzień <mateusz.grudzien.pl@gmail.com>
2018-12-27 11:30:34 +0100

    init commit

---
+++ src/main/java/pl/solidlabs/salaries/SalariesApplication.java
@@ -0,0 +1,14 @@
+package pl.solidlabs.salaries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalariesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalariesApplication.class, args);
+	}
+
+}
+
